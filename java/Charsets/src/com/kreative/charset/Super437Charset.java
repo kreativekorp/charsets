7efe1b4aba40f173b2355037fa7f06abec4d675f
Rebecca G. Bettencourt <beckiejo@gmail.com>
2020-12-18 04:42:32 -0800

    Formatting changes

--- java/Charsets/src/com/kreative/charset/Super437Charset.java
+++ java/Charsets/src/com/kreative/charset/Super437Charset.java
@@ -18,12 +18,14 @@ public class Super437Charset extends Charset {
 	};
 	
 	private static final List<String> KNOWN_SUBSETS = Arrays.asList(
-			"us-ascii", "iso-8859-1", "windows-1252", "macroman", "x-nextstep",
-			"x-kk-superlatin", "x-kk-superroman", "x-kk-supermultinational", "x-kk-super437",
-			"x-kk-superlatin-c0", "x-kk-superroman-c0", "x-kk-supermultinational-c0", "x-kk-super437-c0"
+		"us-ascii", "iso-8859-1", "windows-1252", "macroman", "x-nextstep",
+		"x-kk-superlatin", "x-kk-superlatin-c0",
+		"x-kk-superroman", "x-kk-superroman-c0",
+		"x-kk-supermultinational", "x-kk-supermultinational-c0",
+		"x-kk-super437", "x-kk-super437-c0"
 	);
 	private static final List<String> KNOWN_SUBSETS_C0 = Arrays.asList(
-			"us-ascii", "x-kk-super437-c0"
+		"us-ascii", "x-kk-super437-c0"
 	);
 	
 	private final List<String> knownSubsets;
@@ -31,23 +33,23 @@ public class Super437Charset extends Charset {
 	
 	public Super437Charset(boolean overrideC0) {
 		super(
-				overrideC0 ? CANONICAL_NAME : CANONICAL_NAME_C0,
-				overrideC0 ? ALIASES : ALIASES_C0
+			overrideC0 ? CANONICAL_NAME : CANONICAL_NAME_C0,
+			overrideC0 ? ALIASES : ALIASES_C0
 		);
 		this.knownSubsets = overrideC0 ? KNOWN_SUBSETS : KNOWN_SUBSETS_C0;
 		this.overrideC0 = overrideC0;
 	}
-
+	
 	@Override
 	public boolean contains(Charset cs) {
 		return knownSubsets.contains(cs.name().toLowerCase());
 	}
-
+	
 	@Override
 	public CharsetDecoder newDecoder() {
 		return new Super437Decoder(this, overrideC0);
 	}
-
+	
 	@Override
 	public CharsetEncoder newEncoder() {
 		return new Super437Encoder(this, overrideC0);
--- java/Charsets/src/com/kreative/charset/SuperRomanCharset.java
+++ java/Charsets/src/com/kreative/charset/SuperRomanCharset.java
@@ -18,12 +18,14 @@ public class SuperRomanCharset extends Charset {
 	};
 	
 	private static final List<String> KNOWN_SUBSETS = Arrays.asList(
-			"us-ascii", "iso-8859-1", "windows-1252", "macroman", "x-nextstep",
-			"x-kk-superlatin", "x-kk-superroman", "x-kk-supermultinational", "x-kk-super437",
-			"x-kk-superlatin-c0", "x-kk-superroman-c0", "x-kk-supermultinational-c0", "x-kk-super437-c0"
+		"us-ascii", "iso-8859-1", "windows-1252", "macroman", "x-nextstep",
+		"x-kk-superlatin", "x-kk-superlatin-c0",
+		"x-kk-superroman", "x-kk-superroman-c0",
+		"x-kk-supermultinational", "x-kk-supermultinational-c0",
+		"x-kk-super437", "x-kk-super437-c0"
 	);
 	private static final List<String> KNOWN_SUBSETS_C0 = Arrays.asList(
-			"us-ascii", "macroman", "x-kk-superroman-c0"
+		"us-ascii", "macroman", "x-kk-superroman-c0"
 	);
 	
 	private final List<String> knownSubsets;
@@ -31,23 +33,23 @@ public class SuperRomanCharset extends Charset {
 	
 	public SuperRomanCharset(boolean overrideC0) {
 		super(
-				overrideC0 ? CANONICAL_NAME : CANONICAL_NAME_C0,
-				overrideC0 ? ALIASES : ALIASES_C0
+			overrideC0 ? CANONICAL_NAME : CANONICAL_NAME_C0,
+			overrideC0 ? ALIASES : ALIASES_C0
 		);
 		this.knownSubsets = overrideC0 ? KNOWN_SUBSETS : KNOWN_SUBSETS_C0;
 		this.overrideC0 = overrideC0;
 	}
-
+	
 	@Override
 	public boolean contains(Charset cs) {
 		return knownSubsets.contains(cs.name().toLowerCase());
 	}
-
+	
 	@Override
 	public CharsetDecoder newDecoder() {
 		return new SuperRomanDecoder(this, overrideC0);
 	}
-
+	
 	@Override
 	public CharsetEncoder newEncoder() {
 		return new SuperRomanEncoder(this, overrideC0);
